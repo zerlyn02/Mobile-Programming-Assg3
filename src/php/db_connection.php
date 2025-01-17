8278c0d6c7f9b3f5f8dafef10248cd83b38af4c7
yarchini <v.yarchini@gmail.com>
2025-01-17 23:33:07 +0800

    Update db_connection.php

--- src/php/db_connection.php
+++ src/php/db_connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "12345678";
+$password = "";
 $dbname = "mobile";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
